80a70bf8b6051e76bc32bdf372e690a6dda831b4
dmitriano <winappdev@gmail.com>
2016-08-16 00:10:04 +0300

    Add project files.

---
+++ AppForOpenGLES2/App.xaml.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "app.g.h"
+#include "OpenGLES.h"
+#include "openglespage.xaml.h"
+
+namespace AppForOpenGLES2
+{
+    ref class App sealed
+    {
+    public:
+        App();
+        virtual void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e) override;
+
+    private:
+        OpenGLESPage^ mPage;
+        OpenGLES mOpenGLES;
+    };
+}
