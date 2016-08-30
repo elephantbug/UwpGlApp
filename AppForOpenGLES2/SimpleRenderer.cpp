4855ed93f6ce875f11c5e59baad7bea1edb16001
dmitriano <winappdev@gmail.com>
2016-08-30 04:37:45 +0300

    blending demo

--- AppForOpenGLES2/SimpleRenderer.cpp
+++ AppForOpenGLES2/SimpleRenderer.cpp
@@ -225,7 +225,7 @@ void SimpleRenderer::Draw()
 	//glEnable(GL_BLEND);
 	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	
-	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     if (mProgram == 0)
